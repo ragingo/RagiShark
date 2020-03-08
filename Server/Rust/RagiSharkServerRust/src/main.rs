7aebf4a3d7f4399b8be1d20249280e7467ff4cf2
ragingo <save.the.world.crush@gmail.com>
2020-03-09 08:27:45 +0900

    コード整理

--- Server/Rust/RagiSharkServerRust/src/main.rs
+++ Server/Rust/RagiSharkServerRust/src/main.rs
@@ -1,50 +1,10 @@
-extern crate regex;
-extern crate sha1;
-extern crate base64;
-
 mod platform;
 mod net;
 
 use crate::net::websocket::WebSocketServer;
-use std::io::{Read, Write};
 use std::net::{TcpStream};
 use std::process::Command;
 use std::str;
-use regex::Regex;
-use sha1::{Sha1, Digest};
-
-const WS_GUID: &str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-
-
-fn on_get_request_received(mut stream: &TcpStream, data: &str) {
-    let r = Regex::new("Sec-WebSocket-Key: (.*)").unwrap();
-    let captures = r.captures(data).unwrap();
-    if captures.len() < 2 {
-        return;
-    }
-
-    let m = captures.get(1).unwrap();
-    let key = m.as_str().trim();
-    let mut new_key = key.to_owned();
-    new_key.push_str(WS_GUID);
-
-    let result = Sha1::digest(new_key.as_bytes());
-    let sha1_binary: &[u8] = &result;
-    let sha1_base64 = base64::encode(sha1_binary);
-
-    let header = concat!(
-        "HTTP/1.1 101 Switching Protocols\r\n",
-        "Connection: Upgrade\r\n",
-        "Upgrade: websocket\r\n"
-    );
-
-    let mut send_data = header.to_owned();
-    send_data.push_str(&format!("Sec-WebSocket-Accept: {}\r\n", sha1_base64));
-    send_data.push_str("\r\n");
-    println!("{}", send_data);
-
-    stream.write_all(send_data.as_bytes()).unwrap();
-}
 
 fn on_connected(mut stream: &TcpStream) {
     // let mut s = stream;
@@ -55,19 +15,7 @@ fn on_connected(mut stream: &TcpStream) {
     //         s.write_all(&buf);
     //     }
     // });
-    loop {
-        let mut buf = [0; 1024];
-        let size = stream.read(&mut buf).unwrap();
-        if size == 0 {
-            break;
-        }
-        let data = str::from_utf8(&mut buf[0..size]).unwrap();
-        if data.starts_with("GET /") {
-            on_get_request_received(stream, data);
-        }
-    }
 }
-
 
 fn test() {
     if cfg!(windows) {
--- Server/Rust/RagiSharkServerRust/src/net/websocket.rs
+++ Server/Rust/RagiSharkServerRust/src/net/websocket.rs
@@ -1,4 +1,12 @@
+extern crate regex;
+extern crate sha1;
+extern crate base64;
+
+use std::io::{Read, Write};
 use std::net::{TcpStream, TcpListener, SocketAddrV4, Ipv4Addr};
+use std::str;
+use regex::Regex;
+use sha1::{Sha1, Digest};
 
 pub struct WebSocketHeader {
     fin: bool,
@@ -62,12 +70,45 @@ pub struct WebSocketServer {
     pub client_connect_handler: fn(stream: &mut TcpStream) -> ()
 }
 
+const WS_GUID: &str = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+fn on_get_request_received(mut stream: &TcpStream, data: &str) {
+    let r = Regex::new("Sec-WebSocket-Key: (.*)").unwrap();
+    let captures = r.captures(data).unwrap();
+    if captures.len() < 2 {
+        return;
+    }
+
+    let m = captures.get(1).unwrap();
+    let key = m.as_str().trim();
+    let mut new_key = key.to_owned();
+    new_key.push_str(WS_GUID);
+
+    let result = Sha1::digest(new_key.as_bytes());
+    let sha1_binary: &[u8] = &result;
+    let sha1_base64 = base64::encode(sha1_binary);
+
+    let header = concat!(
+        "HTTP/1.1 101 Switching Protocols\r\n",
+        "Connection: Upgrade\r\n",
+        "Upgrade: websocket\r\n"
+    );
+
+    let mut send_data = header.to_owned();
+    send_data.push_str(&format!("Sec-WebSocket-Accept: {}\r\n", sha1_base64));
+    send_data.push_str("\r\n");
+    println!("{}", send_data);
+
+    stream.write_all(send_data.as_bytes()).unwrap();
+}
+
 impl WebSocketServer {
     pub fn new() -> WebSocketServer {
         WebSocketServer {
             client_connect_handler: |_| ()
         }
     }
+
     pub fn listen(&mut self, port: u16) {
         let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, port);
         let listener = TcpListener::bind(addr).unwrap();
@@ -75,11 +116,27 @@ impl WebSocketServer {
         for stream in listener.incoming() {
             match stream {
                 Ok(mut s) => {
-                    (self.client_connect_handler)(&mut s);
+                    self.on_connected(&mut s);
                 },
                 Err(e) => {
                     println!("{}", e);
                 }
+            }
+        }
+    }
+
+    fn on_connected(&mut self, stream: &mut TcpStream) {
+        (self.client_connect_handler)(stream);
+
+        loop {
+            let mut buf = [0; 1024];
+            let size = stream.read(&mut buf).unwrap();
+            if size == 0 {
+                break;
+            }
+            let data = str::from_utf8(&mut buf[0..size]).unwrap();
+            if data.starts_with("GET /") {
+                on_get_request_received(stream, data);
             }
         }
     }
