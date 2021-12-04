d18d78c296f81f18239e94cd9bc57a7f1aabf7db
ragingo <save.the.world.crush@gmail.com>
2021-12-04 14:40:44 +0900

    [Server] 実装言語を切り替えられるようにした

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
-pushd ./Server
+pushd ./Server > /dev/null
 
 if [[ ! -e .env ]]; then
     echo ".env.sample を参考にして .env を作って！"
@@ -10,12 +10,20 @@ fi
 # shellcheck disable=SC1091
 source .env
 
-readonly APP=./CSharp/RagiSharkServerCS
-readonly APP_ARGS=(
-    "-l" "$LISTEN_ADDR"
-    "-p" "$LISTEN_PORT"
-)
+run_cs() {
+    readonly APP=./CSharp/RagiSharkServerCS
+    readonly APP_ARGS=(
+        "-l" "$LISTEN_ADDR"
+        "-p" "$LISTEN_PORT"
+    )
+    dotnet run --project $APP -- "${APP_ARGS[@]}"
+}
 
-dotnet run --project $APP -- "${APP_ARGS[@]}"
+case "$IMPL_LANG" in
+    CS) run_cs ;;
+    CPP) echo "C++" ;;
+    RS) echo "Rust" ;;
+    SW) echo "Swift" ;;
+esac
 
-popd
+popd > /dev/null
